831ee77ab64a39cab4614c50117beab964f58559
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-10 11:55:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        if (typeof width !== 'number' || typeof height !== 'number') {
+            throw new Error('Width and height must be numbers');
+        }
+        if (width <= 0 || height <= 0) {
+            throw new Error('Width and height must be positive');
+        }
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    get width() {
+        return this._width;
+    }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+    get height() {
+        return this._height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        if (typeof side !== 'number') {
+            throw new Error('Side must be a number');
+        }
+        if (side <= 0) {
+            throw new Error('Side must be positive');
+        }
+        super(side, side);
+    }
+
+    getPerimeter() {
+        return this.width * 4;
+    }
+}
+
+document.getElementById('create-rectangle-button').addEventListener('click', function() {
+    try {
+        const rectangle = new Rectangle(5, 10);
+        console.log(rectangle.width);
+        console.log(rectangle.height);
+        console.log(rectangle.getArea());
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+document.getElementById('create-square-button').addEventListener('click', function() {
+    try {
+        const square = new Square(7);
+        console.log(square.width);
+        console.log(square.height);
+        console.log(square.getArea());
+        console.log(square.getPerimeter());
+    } catch (error) {
+        console.error(error.message);
+    }
+});
